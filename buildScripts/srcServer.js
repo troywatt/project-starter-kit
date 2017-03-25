9007109b3d5f150a32a7a0cc954153e62abfa1c6
troywatt <troywatt@gmail.com>
2017-03-25 09:07:27 -0600

    adds simple express dev server with hello world index.html

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,22 @@
+var express = require( 'express' );
+var path = require( 'path' );
+var open = require( 'open' );
+
+var port = 3001;
+
+
+var app = express();
+
+// routes
+app.get( '/', function( req, res ){
+    res.sendFile( path.join( __dirname, '../src/index.html' ));
+} );
+
+app.listen( port, function( err ){
+    if( err ) {
+        console.log( err );
+    }
+    else {
+        open( 'http://localhost:' + port );
+    }
+});
